6f55be40b0ddb826d6d0fb36a894ad8678e7df1b
Haval-Jalal <Haval.jalal@hotmail.com>
2026-01-18 23:11:25 +0100

    Add project files.

---
+++ Program.cs
@@ -0,0 +1,12 @@
+﻿namespace MiniECommerceDB
+{
+    internal class Program
+    {
+        static void Main(string[] args)
+        {
+            Console.WriteLine("Hello, World!");
+
+           
+        }
+    }
+}
